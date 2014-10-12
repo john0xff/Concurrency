9928083dfad9bbfc56e4eac5ac9171213a11d6e3
BartBien <phoenixjcam@gmail.com>
2014-10-12 08:30:49 +0200

    Concurrent writer - TODO

---
+++ src/com/phoenixjcam/concurrent/files/write/ConcurrentWriter.java
@@ -0,0 +1,7 @@
+package com.phoenixjcam.concurrent.files.write;
+
+public class ConcurrentWriter
+{
+	// write some random tex to file with 2 - 5 thread at the same time without synchronization and locks
+	// the same with synchro and locks
+}
