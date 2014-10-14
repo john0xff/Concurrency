c3f9820f1b66672c687006183c70734e667344ca
BartBien <phoenixjcam@gmail.com>
2014-10-14 09:03:40 +0200

    jdk 1.5 - atomic types

---
+++ src/atomic/AtomicExample.java
@@ -0,0 +1,12 @@
+package atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicExample
+{
+	public static void main(String[] args)
+	{
+		AtomicInteger atomicInteger = new AtomicInteger(2);
+		atomicInteger.intValue();
+	}
+}
