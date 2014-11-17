d6007ba1e347222df379938bd729f8f612d32fd7
BartBien <phoenixjcam@gmail.com>
2014-11-17 21:32:57 +0100

    update of max threading apps - 10 000 threads + caret text area

--- maxThreads/maxThread/MaxThreadAmount.java
+++ maxThreads/maxThread/MaxThreadAmount.java
@@ -5,63 +5,70 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-
-
+import javax.swing.text.DefaultCaret;
 
 public class MaxThreadAmount
 {
 	private JFrame frame;
 	private JScrollPane scrollPane;
 	private JTextArea textArea;
-	
+
 	public MaxThreadAmount()
 	{
 		frame = new JFrame("max thread amount");
 		frame.setSize(600, 400);
 		frame.setLocation(200, 200);
-		
+
 		textArea = new JTextArea();
+		DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		
 		scrollPane = new JScrollPane(textArea);
-		
-		frame.add(scrollPane,  BorderLayout.CENTER);
-		
+
+		frame.add(scrollPane, BorderLayout.CENTER);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
-	
+
 	public JTextArea getTextArea()
 	{
 		return this.textArea;
 	}
-	
+
 	public static void main(String[] args)
 	{
 		MaxThreadAmount maxThreadAmount = new MaxThreadAmount();
-		
-		for (int i = 0; i < 1000; i++)
+
+		for (int i = 0; i < 10000; i++)
 		{
 			new Thread(new Runnable()
 			{
-				
+
 				@Override
 				public void run()
 				{
-					for (int j = 0; j < 100000; j++)
+					// for (int j = 0; j < 100000; j++)
+					// {
+
+					while (true)
 					{
 						try
 						{
 							maxThreadAmount.getTextArea().append("test -> " + Thread.currentThread().getId() + "\n");
-							Thread.sleep(10);
+							Thread.sleep(1000);
+							
+							
 						}
 						catch (InterruptedException e)
 						{
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					}
+					// }
 				}
 			}).start();
 		}
-		
+
 	}
 }
---
+++ utilConcurrent/executor/StackCallTomcat.java
@@ -0,0 +1,26 @@
+package executor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class StackCallTomcat
+{
+
+	public static void main(String[] args)
+	{
+		// tomcat at the beginning of each request
+		
+		// java.lang.Thread
+		// java.util.concurrent.ThreadPoolExecutor
+		// ~
+		// ~
+		// javax.servlet.http.HttpServlet 
+		// org.springframework.web.servlet.FrameworkServlet
+		// org.springframework.web.servlet.DispatcherServlet
+		// org.springframework.web.servlet.mvc.method.AbstractHandlerMethodAdapter
+		// org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod
+		// java.lang.reflect.Method 
+		// sun.reflect.DelegatingMethodAccessorImpl
+		// sun.reflect.NativeMethodAccessorImpl
+	}
+
+}
